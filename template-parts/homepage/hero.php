079175e6613b6218cca3d1c09a856c532912a4ba
DevNemanja <89532695+DevNemanja@users.noreply.github.com>
2024-12-09 14:26:28 +0100

    hero

---
+++ template-parts/homepage/hero.php
@@ -0,0 +1,8 @@
+<header class="hero" style="background-image: url('<?php echo esc_url($naslovna_slika); ?>');">
+        <?php if ($logo) : ?>
+            <div class="hero-logo">
+                <img src="<?php echo esc_url($logo); ?>" alt="<?php bloginfo('name'); ?> Logo">
+            </div>
+        <?php endif; ?>
+
+</header>

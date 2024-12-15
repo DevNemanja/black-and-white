a4e3dd4657ccb21951766d212dffe7e5c06a47ed
DevNemanja <89532695+DevNemanja@users.noreply.github.com>
2024-12-15 15:14:48 +0100

    single product page

--- single-product.php
+++ single-product.php
@@ -71,7 +71,7 @@ get_header(); ?>
                         ?>
                     </div>
                 </div>
-                <div>
+                <div class="single-product-content-wrapper">
                         <h1><?php the_title(); ?></h1>
                         <div class="single-product-content">
                             <?php the_content(); ?>
@@ -129,6 +129,51 @@ get_header(); ?>
                             }
                         ?>  
                     </div>
+            </div>
+            <div class="single-product-details-section">
+                <div>
+                    <h3 class="single-product-details-title">GOLDEN SATIN FABRIC123</h3>
+                    <div>
+                        <?php echo apply_filters( 'woocommerce_short_description', $product->get_short_description() ); ?>
+                    </div>
+                </div>
+                <div>
+                    <h3 class="single-product-details-title">Specifications123</h3>
+                    <div>
+                        <?php echo apply_filters( 'woocommerce_short_description', $product->get_short_description() ); ?>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <?php
+                    global $product;
+
+                    $upsells = $product->get_upsell_ids(); // Get the upsell product IDs
+
+                    if ( ! empty( $upsells ) ) {
+                        echo '<div class="single-product-upsells">';
+                        echo '<h2>You might also like</h2>';
+                        echo '<ul class="single-product-upsell-products">';
+
+                        foreach ( $upsells as $upsell_id ) {
+                            $upsell_product = wc_get_product( $upsell_id );
+
+                            if ( $upsell_product ) {
+                                echo '<li>';
+                                echo '<a href="' . esc_url( get_permalink( $upsell_product->get_id() ) ) . '">';
+                                echo $upsell_product->get_image(); // Display the upsell product image
+                                echo '<h3>' . esc_html( $upsell_product->get_name() ) . '</h3>';
+                                echo '</a>';
+                                echo '</li>';
+                            }
+                        }
+
+                        echo '</ul>';
+                        echo '</div>';
+                    } else {
+                        echo '';
+                    }
+                ?>
             </div>
         </section>
     </div>

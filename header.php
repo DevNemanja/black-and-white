ca281dbff1d16a3e398bec8043384d3b9f0dbb6e
Nemanja Blagojevic <dev.nemanja@gmail.com>
2025-01-08 16:36:57 +0100

    Prebaci product category u template

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 		<div class="container">
 			<div class="header">
 				<div>
-					<a href="/">
+					<a href="<?php echo esc_url(home_url(null, get_locale())); ?>">
 						<img src="<?php echo esc_url(get_theme_mod('footer_logo')); ?>" alt="Logo" class="header-logo">
 					</a>
 				</div>

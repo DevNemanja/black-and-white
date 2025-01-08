a74ca9f5388866c346a51da878f6651052c215d8
Nemanja Blagojevic <dev.nemanja@gmail.com>
2025-01-08 23:44:58 +0100

    Add social links to mobile nav

--- header.php
+++ header.php
@@ -47,10 +47,44 @@
 							)
 						);
 					?>
-					<img class="mobile-nav-logo" src="<?php echo get_template_directory_uri(); ?>/images/black-logo.svg" />
+					<div class="mobile-nav-addition">
+						<img class="mobile-nav-logo" src="<?php echo get_template_directory_uri(); ?>/images/black-logo.svg" />
+						
+						<?php
+							$facebook_link = get_theme_mod('facebook_link');
+							$instagram_link = get_theme_mod('instagram_link');
+							$email_link = get_theme_mod('email_link');
 
+							$facebook_icon = get_theme_mod('facebook_icon');
+							$instagram_icon = get_theme_mod('instagram_icon');
+							$email_icon = get_theme_mod('email_icon');
+						?>
+						
+						<ul class="social-icons-wrapper">
+							<?php if ($facebook_link && $facebook_icon): ?>
+								<li class="social-icon">
+									<a href="<?php echo esc_url($facebook_link); ?>" target="_blank" rel="noopener noreferrer">
+										<img src="<?php echo esc_url($facebook_icon); ?>" alt="Facebook Icon">
+									</a>
+								</li>
+							<?php endif; ?>
+							<?php if ($instagram_link && $instagram_icon): ?>
+								<li class="social-icon">
+									<a href="<?php echo esc_url($instagram_link); ?>" target="_blank" rel="noopener noreferrer">
+										<img src="<?php echo esc_url($instagram_icon); ?>" alt="instagram Icon">
+									</a>
+								</li>
+							<?php endif; ?>
+							<?php if ($email_link && $email_icon): ?>
+								<li class="social-icon">
+									<a href="<?php echo esc_url($email_link); ?>" target="_blank" rel="noopener noreferrer">
+										<img src="<?php echo esc_url($email_icon); ?>" alt="email Icon">
+									</a>
+								</li>
+							<?php endif; ?>
+						</ul>
+					</div>
 				</nav>
 			</div>
-
 		</div>
 	</header> 

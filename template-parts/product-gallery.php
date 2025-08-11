796e2f0f734d24e0a321ed9b84d5632b23c44615
Nemanja Blagojevic <dev.nemanja@gmail.com>
2025-08-11 20:46:40 +0200

    Add zoom

--- template-parts/product-gallery.php
+++ template-parts/product-gallery.php
@@ -11,7 +11,7 @@ if (class_exists('WooCommerce')) {
 }
 ?>
 
-<div class="<?php echo esc_attr($containerClass); ?>">
+<div class="<?php echo esc_attr($containerClass); ?> images-wrapper">
     <?php if (!empty($gallery_image_ids)) : ?>
         <!-- Thumbnail Pagination Swiper -->
         <div class="single-product-swiper-pagination">
@@ -39,7 +39,9 @@ if (class_exists('WooCommerce')) {
                     </div>
                 <?php endforeach; ?>
             </div>
+            
         </div>
+        
     <?php else : ?>
         <!-- Just the Featured Image -->
         <?php if ($featured_image_id) :
@@ -51,9 +53,100 @@ if (class_exists('WooCommerce')) {
             </div>
         <?php endif; ?>
     <?php endif; ?>
+    <button class="zoom-button">
+        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    </button>
+    <div class="modal-wrapper hidden">
+        <div class="overlay"></div>
+        <div class="image-container"></div>
+    </div>
 </div>
 
+<script>
+    const overlay = document.querySelector('.overlay');
+    const modalWrapper = document.querySelector('.modal-wrapper');
+
+    overlay.addEventListener('click', () => {
+        modalWrapper.classList.add('hidden');
+    });
+
+    document.querySelector('.zoom-button').addEventListener('click', () => {
+        const mainImage = document.querySelector('.single-product-main-image img');
+        const activeImage = document.querySelector('.swiper-slide-active img');
+        const modalImageContainer = document.querySelector('.image-container');
+
+        modalImageContainer.innerHTML = `<img src="${mainImage?.src ?? activeImage?.src}" alt="${mainImage?.alt ?? activeImage?.alt}" />`;
+        modalWrapper.classList.remove('hidden');
+    });
+
+</script>
+
 <style>
+    .modal-wrapper.hidden {
+        opacity: 0;
+        z-index: -1;
+    }
+
+    .modal-wrapper {
+        border: 1px solid #000;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 1000;
+        transition: opacity 0.2s;
+        z-index: 1000;
+    }
+
+    .overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.6);
+        z-index: -1;
+    }
+
+    .image-container {
+        width: 100%;
+        height: 100%;
+        max-width: 80vw;
+        max-height: 80vh;
+    }
+
+    .images-wrapper {
+        position: relative;
+    }
+
+    .zoom-button {
+        position: absolute;
+        bottom: 10px;
+        right: 10px;
+        background-color: rgba(255, 255, 255, 0.8);
+        border: none;
+        padding: 10px;
+        cursor: pointer;
+        z-index: 10;
+        border-radius: 50%;
+        transition: opacity 0.2s;
+        opacity: 0.8
+    }
+    
+    .zoom-button:hover {
+        opacity: 1
+    }
+
+    .single-product-main-image img {
+        display: block;
+    }
+
     .single-product-swiper {
         width: 100%;
         height: 100%;

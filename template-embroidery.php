00398ac78c835f10e33653ff211a69054043c818
Nemanja Blagojevic <nblagojevic@aimconsulting.com>
2025-09-13 15:06:18 +0200

    Dodaj galeriju u vezionicu

--- template-embroidery.php
+++ template-embroidery.php
@@ -1,34 +1,36 @@
 <?php
+
 /**
  * Template Name: Embroidery Page Template
  * Description: A custom template for displaying the Embroidery page with the title based on the current language.
  */
 
-    // Load WordPress header
-    get_header();
+// Load WordPress header
+get_header();
 
-    $naslov = get_field('naslov');
-    $podnaslov = get_field('podnaslov');
-    $naslovna_slika = get_field('glavna_slika');
+$naslov = get_field('naslov');
+$podnaslov = get_field('podnaslov');
+$naslovna_slika = get_field('glavna_slika');
 
-    $title = get_field('sekundarni_naslov');
-    $text = get_field('opis_ispod_sekundarnog_naslova');
+$title = get_field('sekundarni_naslov');
+$text = get_field('opis_ispod_sekundarnog_naslova');
 
-    require('template-parts/hero.php');
+require('template-parts/hero.php');
 
-    $tekstIznadNaslova = get_field('tekst_iznad_sekundarnog_naslova');
-    $sekNaslov = get_field('sekundarni_naslov');
-    $slikaIza = get_field('slika_iza');
-    $slikaIspred = get_field('slika_ispred');
-    $naslovZaDveSlike = get_field('naslov_za_dve_slike');
-    $tekstDveSlike = get_field('tekst_dve_slike');
-    $poklonTekst = get_field('poklon_tekst');
-    $logoTekst = get_field('logo_tekst');
-    $majicaTekst = get_field('majica_tekst');
+$tekstIznadNaslova = get_field('tekst_iznad_sekundarnog_naslova');
+$sekNaslov = get_field('sekundarni_naslov');
+$slikaIza = get_field('slika_iza');
+$slikaIspred = get_field('slika_ispred');
+$naslovZaDveSlike = get_field('naslov_za_dve_slike');
+$tekstDveSlike = get_field('tekst_dve_slike');
+$poklonTekst = get_field('poklon_tekst');
+$logoTekst = get_field('logo_tekst');
+$majicaTekst = get_field('majica_tekst');
 
-    require('template-parts/overlapping-images-section.php');
-    require('template-parts/embroidery-slider-section.php');
-    
+require('template-parts/overlapping-images-section.php');
+require('template-parts/embroidery-slider-section.php');
+
+
 
 // Load WordPress footer
 get_footer();
--- template-parts/embroidery-slider-section.php
+++ template-parts/embroidery-slider-section.php
@@ -1,7 +1,6 @@
 <?php
-    $naslov = get_field('slider_naslov');
-    $tekst = get_field('slider_tekst');
-
+$naslov = get_field('slider_naslov');
+$tekst = get_field('slider_tekst');
 ?>
 
 <section class="embroidery-slider">
@@ -9,22 +8,35 @@
         <h3 class="embroidery-slider-title"><?php echo $naslov; ?></h3>
         <p class="embroidery-slider-text"><?php echo $tekst; ?></p>
         <div class="embroidery-image-gallery">
-            <div class="embroidery-swiper swiper">
-                <div class="swiper-wrapper">
-                    <?php
-                    for ($i = 1; $i <= 10; $i++) {
-                        $image_url = get_field("image{$i}");
-                        if ($image_url) {
-                            echo '<div class="swiper-slide"><img src="' . esc_url($image_url) . '" alt="Slider image ' . $i . '"></div>';
-                        }
-                    }
-                    ?>
-                </div>
-                <!-- Add Pagination -->
-                <div class="swiper-pagination">123</div>
+            <?php if (have_posts()) : while (have_posts()) : the_post();
+                    $content = get_the_content();
 
-            </div>
+                    if (!empty($content)) {
+                        // Omogućava parsiranje HTML-a
+                        libxml_use_internal_errors(true);
+                        $doc = new DOMDocument();
+                        $doc->loadHTML('<?xml encoding="utf-8" ?>' . $content);
+
+                        // Pronađi sve <img> tagove
+                        $images = $doc->getElementsByTagName('img');
+            ?>
+                        <div class="embroidery-swiper swiper">
+                            <div class="swiper-wrapper">
+                                <?php
+                                foreach ($images as $index => $image) {
+                                    $image_url = $image->getAttribute('src');
+                                    $alt_text = $image->getAttribute('alt');
+                                    if ($image_url) {
+                                        echo '<div class="swiper-slide"><img src="' . esc_url($image_url) . '" alt="' . esc_attr($alt_text ?: 'Slider image ' . ($index + 1)) . '"></div>';
+                                    }
+                                }
+                                ?>
+                            </div>
+                            <div class="swiper-pagination"></div>
+                        </div>
+            <?php }
+                endwhile;
+            endif; ?>
         </div>
     </div>
-
 </section>
